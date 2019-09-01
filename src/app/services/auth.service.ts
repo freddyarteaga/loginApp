d36ffc3550956439ba0a8c35b8f846c5b3d89c4d
Freddy <freddy.artea@gmail.com>
2019-09-01 12:45:17 -0500

    config auth service with guard and localstorage

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -77,8 +77,15 @@ export class AuthService {
   }
 
   private guardarToken( idToken: string ){
+
     this.userToken = idToken;
     localStorage.setItem('token', idToken);
+
+    let hoy = new Date();
+    hoy.setSeconds( 3600 );
+
+    localStorage.setItem('expira', hoy.getTime().toString());
+
   }
 
   leerToken() {
@@ -92,7 +99,25 @@ export class AuthService {
   }
 
   estaAutenticado() : boolean {
+    // return this.userToken.length > 2;
+
+    if ( this.userToken.length < 2 ) {
+      return false
+    }
+
+    const expira = Number(localStorage.getItem('expira'));
+    const expiraDate = new Date()
+    expiraDate.setTime(expira);
+
+    if ( expiraDate > new Date() ) {
+      return true;
+    }else {
+      return false;
+    }
+
     return this.userToken.length > 2;
+
+  }
   }
 
-}
+
